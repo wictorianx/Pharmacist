f972e1a087cfcda4c709ee0bb462fd899eed20aa
wictorianx <88106599+wictorianx@users.noreply.github.com>
2023-01-18 20:29:12 +0300

    Delete pharmapp  frontend directory

--- pharmapp  frontend/conn.py
+++
@@ -1,8 +0,0 @@
-import socket
- 
-serverMACAddress = '98:D3:32:21:18:6D'
-port = 4
-s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.connect((serverMACAddress,port))
-s.send(bytes("F"))
-s.close()
